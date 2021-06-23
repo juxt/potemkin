d9019d46c03ead68679c09d3eb541fe4ffaecbe9
James Henderson <james@jarohen.me.uk>
2021-06-23 17:44:33 +0100

    mirror to mvn central

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
-(defproject potemkin "0.4.5"
+(defproject pro.juxt.clojars-mirrors.clj-commons/potemkin "0.4.5"
   :license {:name "MIT License"}
-  :description "Some useful facades."
-  :dependencies [[clj-tuple "0.2.2"]
-                 [riddley "0.1.12"]]
+  :description "Some useful facades (mirrored from Clojars by JUXT)"
+  :url "https://github.com/clj-commons/potemkin"
+  :scm {:name "git"
+        :url "https://github.com/juxt/potemkin"
+        :dir "."}
+  :dependencies [[pro.juxt.clojars-mirrors.ztellman/clj-tuple "0.2.2"]
+                 [pro.juxt.clojars-mirrors.ztellman/riddley "0.2.0"]]
   :profiles {:dev {:dependencies [[criterium "0.4.3"]
                                   [collection-check "0.1.6"]]}
              :provided {:dependencies [[org.clojure/clojure "1.8.0-RC4"]]}}
@@ -14,4 +18,22 @@
   :java-source-paths ["src"]
   :jvm-opts ^:replace ["-server" "-Xmx4g"]
   :javac-options ["-source" "1.6" "-target" "1.6"]
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+
+  ;; empty JARs to keep Maven Central happy
+  :classifiers {:sources {:jar-exclusions [#""]}
+                :javadoc {:jar-exclusions [#""]}}
+
+  :pom-addition ([:developers
+                  [:developer
+                   [:id "ztellman"]
+                   [:name "Zach Tellman"]]
+                  [:developer
+                   [:id "juxt"]
+                   [:name "JUXT"]
+                   [:roles [:role "packager"]]]])
+
+  :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+                                    :creds :gpg}
+                        "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots"
+                                     :creds :gpg}})
